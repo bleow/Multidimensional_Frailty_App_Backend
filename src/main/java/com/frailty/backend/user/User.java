2a549377f4eea8b76d5488042bcd3c3e0e0624bd
Randomystick <bryankhleow@gmail.com>
2022-10-13 09:26:29 +0800

    init

---
+++ src/main/java/com/frailty/backend/user/User.java
@@ -0,0 +1,69 @@
+package com.frailty.backend.user;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+
+public class User implements UserDetails {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private UserRole userRole;
+    private Boolean isLocked;
+    private Boolean isEnabled;
+
+    
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(userRole.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !isLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+}
---
+++ src/main/java/com/frailty/backend/user/UserRole.java
@@ -0,0 +1,7 @@
+package com.frailty.backend.user;
+
+public enum UserRole {
+    PATIENT,
+    CLINICIAN,
+    ADMIN
+}
